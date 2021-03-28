928018fc4396d473c81c2b3c980c10130da071d7
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2021-03-28 12:02:08 +0200

    day7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from '@emotion/styled';
 
 import './App.css';
 
@@ -53,33 +54,47 @@ PokemonInfo.propTypes = {
 	})
 };
 
+const Title = styled.h1`
+	text-align: center;
+`;
+const TwoColumnLayout = styled.div`
+	display: grid;
+	grid-template-columns: 70% 30%;
+	grid-column-gap: 1rem;
+`;
+const Container = styled.div`
+	margin: auto;
+	width: 800;
+	padding-top: 1rem;
+`;
+const Input = styled.input`
+	width: 100%;
+	font-size: x-large;
+	padding: 0.2rem;
+`;
+
 function App() {
 	const [filter, filterSet] = React.useState('');
 	const [pokemon, pokemonSet] = React.useState([]);
 	const [selectedItem, selectedItemSet] = React.useState(null);
 
 	React.useEffect(() => {
-		fetch('http://localhost:3000/starting-react/pokemon.json')
+		let host =
+			!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+				? 'http://localhost:3000'
+				: '';
+
+		fetch(`${host}/starting-react/pokemon.json`)
 			.then(resp => resp.json())
 			.then(data => pokemonSet(data));
 	}, []);
 
 	return (
-		<div
-			style={{
-				margin: 'auto',
-				width: 800,
-				paddingTop: '1rem'
-			}}>
-			<h1 className='title'>Pokemon Search</h1>
-			<div
-				style={{
-					display: 'grid',
-					gridTemplateColumns: '70% 30%',
-					girdColumnGap: '1rem'
-				}}>
+		<Container>
+			<Title>Pokemon Search</Title>
+			<TwoColumnLayout>
 				<div>
-					<input value={filter} onChange={evt => filterSet(evt.target.value)} />
+					<Input value={filter} onChange={evt => filterSet(evt.target.value)} />
 					<table width='100%'>
 						<thead>
 							<tr>
@@ -103,8 +118,8 @@ function App() {
 					</table>
 				</div>
 				{selectedItem && <PokemonInfo {...selectedItem} />}
-			</div>
-		</div>
+			</TwoColumnLayout>
+		</Container>
 	);
 }
 
